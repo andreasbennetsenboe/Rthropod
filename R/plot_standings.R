ec0ed067255bf1bb0f8f92fa405b40d61a9834a2
andreasbennetsenboe <andreas.b.boe@gmail.com>
2021-06-24 20:05:00 +0200

    Fixed x axis labels

--- R/plot_standings.R
+++ R/plot_standings.R
@@ -28,5 +28,6 @@ plot_standings <- function(x) {
       position = ggplot2::position_dodge(0.5), 
       size=3
     ) +
-    ggplot2::theme_minimal()
+    ggplot2::theme_minimal()+ 
+    theme(axis.text.x=element_text(angle=45,hjust=1))
 }
